841d99be84ec22d1b7c6531d8393cb9507111cb5
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-07 07:13:18 +0000

    Renamed test 'assignment' to be called 'number'.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -3,9 +3,9 @@ mod tests {
     use std::any::Any;
 
     #[test]
-    fn assignment() {
+    fn number() {
         use crate::parser::*;
         let result = lexer(&String::from("1.8e6")).pop().unwrap();
-        assert_eq!(result, TokenValue::Literal(LiteralValue::Number(Number::from_values(&Some(String::from("6")), &Some(String::from("8")), &String::from("1"))), Loc::new(1, 5)))
+        assert_eq!(result, TokenValue::Literal(LiteralValue::Number(Number::from_values(&Some(String::from("6")), &Some(String::from("8")), &String::from("1"))), Loc::new(1, 5)));
     }
 }

7504ef762b2985e4a16c60f52b06c5db46dbd2c8
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-08 15:26:56 +0000

    Added matching for commas in the lexer.

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -398,6 +398,10 @@ pub fn lexer(input: &String) -> Vec<TokenValue> {
             ' ' => {
                 get_next(&mut input_stack);
             }
+            ',' => {
+                get_next(&mut input_stack);
+                output_stack.push(TokenValue::new(Token::Comma, loc.line_num, loc.column_num))
+            }
             '=' => {
                 get_next(&mut input_stack);
                 loc.column_num += 1;

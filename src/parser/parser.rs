4df1256929c264f721ebcadaf284f44e66c146a4
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-09 09:10:55 +0000

    Added code for parsing if statements to the parser.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,5 +1,5 @@
 use crate::parser::lexer::{Keyword, Loc, Operator, Token, TokenValue};
-use indextree::Arena;
+use indextree::{Arena, NodeId};
 use std::any::Any;
 
 pub enum Condition {
@@ -17,6 +17,7 @@ pub enum Item {
     Condition(Condition),
     // The `String` is the variable name
     Variable(String),
+    Expression,
 }
 
 pub struct Node {
@@ -24,27 +25,43 @@ pub struct Node {
     item: Item,
 }
 
+impl Node {
+    pub fn new(item: Item, loc: Loc) -> Self {
+        Self {
+            item,
+            loc,
+        }
+    }
+}
+
 fn parse_boolean() {}
 
 fn parse_arithmetic() {}
 
-fn parse_expression() {}
+fn parse_assignment() {}
 
-fn parse_if(parent: &mut Arena<Node>, tokens: &mut Vec<TokenValue>) {}
+fn parse_expression(parent: &NodeId, arena: &mut Arena<Node>, tokens: &mut Vec<TokenValue>) {}
 
-fn parse_function() {}
+fn parse_if(parent: &NodeId, arena: &mut Arena<Node>, tokens: &mut Vec<TokenValue>) {
+    let expression = arena.new_node(Node::new(Item::Expression, tokens.iter().next().unwrap().loc));
+    parent.append(expression, arena);
+    parse_expression(&expression, arena, tokens);
+}
 
 fn parse_while() {}
 
 fn parse_for() {}
 
-fn parse_assignment() {}
+fn parse_function() {}
 
-fn parse_statement(parent: &mut Arena<Node>, tokens: &mut Vec<TokenValue>) {
-    let mut lexitem = tokens.iter().next().unwrap();
+
+fn parse_statement(parent: &NodeId, arena: &mut Arena<Node>, tokens: &mut Vec<TokenValue>) {
+    let mut lexitem = tokens.pop().unwrap();
     match lexitem.token {
         Token::Keyword(Keyword::If) => {
-            parse_if(parent, tokens);
+            let new_node = arena.new_node(Node::new(Item::If, lexitem.loc));
+            parent.append(new_node, arena);
+            parse_if(&new_node, arena, tokens);
         }
         _ => panic!(
             "Unexpected term on line {}, column {}.",

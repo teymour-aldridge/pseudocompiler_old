5ea7a76fda9a619c1bbc90348f123f8a64848a3d
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-17 12:07:07 +0000

    Used `react-app-rewired` to configure a custom loaded suitable for WebAssembly.

--- app/src/editor/Editor.js
+++ app/src/editor/Editor.js
@@ -5,19 +5,52 @@ import "ace-builds/src-noconflict/mode-batchfile";
 import "ace-builds/src-noconflict/theme-github";
 
 class Editor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: "",
+            wasm: {}
+        };
+        this.loadWasm = this.loadWasm.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadWasm();
+    }
+
+    loadWasm = async () => {
+        try {
+            const wasm = await import('pseudocompiler');
+            this.setState({wasm: wasm});
+        } catch (err) {
+            console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+        }
+    };
+
     render() {
+        const wasm = this.state.wasm;
         return <>
             <div className="Editor">
                 <AceEditor
                     mode="batchfile"
-                    width={window.outerWidth < 500 ? window.outerWidth - 50 : window.outerWidth/2}
+                    width={window.outerWidth < 500 ? (window.outerWidth - 50).toString() + 'px' : (window.outerWidth / 2).toString() + 'px'}
                     theme="github"
                     name="editor"
+                    value={this.state.code}
+                    onChange={value => {
+                        this.setState({
+                            code: value
+                        })
+                    }}
                     editorProps={{$blockScrolling: true}}/>
-
             </div>
             <div className="ControlPanel">
-                <button>RUN CODE</button>
+                <button onClick={event => {
+                    event.preventDefault();
+                    eval(wasm.compile(""));
+                }
+                }>RUN CODE
+                </button>
             </div>
         </>
     }

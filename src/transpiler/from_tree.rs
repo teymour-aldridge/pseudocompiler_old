684c9c0c4e7927312fa3577c7320766d77d13ef6
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-18 09:08:54 +0000

    Added parameters to function `from_tree`

--- src/transpiler/from_tree.rs
+++ src/transpiler/from_tree.rs
@@ -1,17 +1,17 @@
 use crate::parser::{parser, parser::Node};
 use indextree::{Arena, NodeId};
 
-fn transpile_block() {}
+fn transpile_block(block_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
-fn transpile_if() {}
+fn transpile_if(if_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
-fn transpile_expression() {}
+fn transpile_expression(expression_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
-fn transpile_function() {}
+fn transpile_function(function_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
-fn transpile_while() {}
+fn transpile_while(while_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
-fn transpile_for() {}
+fn transpile_for(for_node: &NodeId, arena: &mut Arena<Node>, output: &mut String) {}
 
 pub fn from_tree(tree: Arena<Node>, start_node: NodeId) -> String {
     let mut output = String::new();

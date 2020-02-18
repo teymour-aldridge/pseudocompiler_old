93f8085cfdf2b93b530dc46015025ebc89b89da0
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-02-18 19:23:55 +0000

    Renamed variable `tree` to arena. Also improved the formatting of the code using cargo fmt.

--- src/tests/parser.rs
+++ src/tests/parser.rs
@@ -1,20 +1,20 @@
 use crate::parser::{lexer::Number, *};
 
 #[test]
-fn test_parser() {
-	let (tree, initial_node) = parser::parser(&mut lexer::lexer(&String::from("x=1")));
-	let x = tree.get(initial_node).unwrap();
-	let assign_operator_node = tree.get(x.first_child().unwrap()).unwrap().get();
+fn test_assignment() {
+	let (arena, initial_node) = parser::parser(&mut lexer::lexer(&String::from("x=1")));
+	let x = arena.get(initial_node).unwrap();
+	let assign_operator_node = arena.get(x.first_child().unwrap()).unwrap().get();
 	assert!(assign_operator_node.item == parser::Item::Assign);
 	let assign_operator = x.first_child().unwrap();
-	let identifier_node = tree.get(assign_operator).unwrap().first_child().unwrap();
-	let identifier = tree.get(identifier_node).unwrap().get();
+	let identifier_node = arena.get(assign_operator).unwrap().first_child().unwrap();
+	let identifier = arena.get(identifier_node).unwrap().get();
 	assert!(identifier.item == parser::Item::Identifier(String::from("x")));
-	let expression_node = tree.get(assign_operator).unwrap().last_child().unwrap();
-	let expression = tree.get(expression_node).unwrap().get();
+	let expression_node = arena.get(assign_operator).unwrap().last_child().unwrap();
+	let expression = arena.get(expression_node).unwrap().get();
 	assert!(expression.item == parser::Item::Expression);
-	let number_node = tree.get(expression_node).unwrap().last_child().unwrap();
-	let number = tree.get(number_node).unwrap().get();
+	let number_node = arena.get(expression_node).unwrap().last_child().unwrap();
+	let number = arena.get(number_node).unwrap().get();
 	assert!(
 		number.item
 			== parser::Item::Number(Number {
